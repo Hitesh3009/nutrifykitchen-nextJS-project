29037286373b9d470db8d29e8e06213592ccacb6
Hitesh3009 <hiteshbhosle3009@gmail.com>
2024-07-31 00:38:33 +0530

    server side rendering is done so that all the blogs and the individual blog page gets populated from the server side itself

--- app/blogpost/[slug]/page.jsx
+++ app/blogpost/[slug]/page.jsx
@@ -1,27 +1,23 @@
-"use client";
 import Navbar from "@/components/Navbar";
-import { useState,useEffect } from "react";
-export default function Blogpost({ params }) {
-  const [blog, setBlog] = useState();
+
+export async function getMyBlog(slug){
+  const res=await fetch(`http://localhost:3000/api/getblogs?slug=${slug}`,{
+    method: 'GET'
+  });
+  const data= res.json();
+  return data;
+}
+export default async function Blogpost({params}) {
   const slug=params.slug;
-  const fectchBlog = async () => {
-      const parsed = await fetch(`http://localhost:3000/api/getblogs?slug=${slug}`);
-      const data=await parsed.json();
-      setBlog(data);
-  }
-
-  useEffect(()=>{
-    if(slug.length>0)
-    fectchBlog();
-  },[]);
-
+  const myBlog=await getMyBlog(slug);
+  // console.log(myBlog);
   return (<>
     <Navbar />
-    {blog && <div className="main flex justify-center">
+    {myBlog && <div className="main flex justify-center">
       <div className="container flex flex-col items-center border-2 border-black mt-6 w-7/12 p-4">
-        <h1 className="text-4xl font-bold mb-3">{blog.title}</h1>
+        <h1 className="text-4xl font-bold mb-3">{myBlog.title}</h1>
         <hr className="border-2 border-black w-full" />
-        <p className="m-2 text-justify">{blog.description}</p>
+        <p className="m-2 text-justify">{myBlog.description}</p>
       </div>
     </div>}
   </>)
--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,21 +1,16 @@
-"use client";
 import Navbar from '@/components/Navbar'
 import Link from 'next/link'
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-const BlogsPage = () => {
 
-  const [blog, setBlog] = useState([]);
-  const fectchBlog = async () => {
-    const parsed = await fetch('../api/blogs');
-    const data = await parsed.json();
-    setBlog(data);
-  }
-
-  useEffect(() => {
-    fectchBlog();
-  }, []);
-
+export async function getBlogsFromServer(){
+    const res= await fetch('http://localhost:3000/api/blogs',{
+      method: 'GET'
+    });
+    const data=res.json();
+    
+    return data;
+}
+const BlogsPage = async() => {
+    const allBlogs= await getBlogsFromServer();
   return (
     <>
       <Navbar />
@@ -23,7 +18,7 @@ const BlogsPage = () => {
         <p></p>
 
         {
-          blog.map((blogItem) => {
+          allBlogs.map((blogItem) => {
             return (<div className="blogsItems border-2 border-black p-5 w-5/12 h-40" key={blogItem.slug}>
               <div>
                 <Link href={`blogpost/${blogItem.slug}`}><h4 className="text-lg font-bold cursor-pointer">{blogItem.title}</h4></Link>
