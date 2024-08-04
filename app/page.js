3dbaf7650f43bf8a2a0228e60efa2ec26f2ae335
Hitesh3009 <hiteshbhosle3009@gmail.com>
2024-08-05 00:47:15 +0530

    styled the home page and added the images into the public folder

--- app/page.js
+++ app/page.js
@@ -6,35 +6,41 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center pt-16">
+      <main className="flex min-h-screen flex-col items-center pt-16 bg-[url('https://getwallpapers.com/wallpaper/full/2/a/f/1224070-most-popular-plain-hd-wallpapers-1920x1080.jpg')] bg-no-repeat bg-cover">
         <div className="flex flex-col items-center space-y-5">
-          <h2 className="font-bold text-5xl">HUNTING CODER</h2>
-          <div className="imageWrap w-96 h-60 relative">
-            <Image src='/coderImg.jpg' alt="Image of a coder" className="rounded-lg p-3" fill />
+          <h2 className="font-bold text-2xl lg:text-3xl xl:text-5xl text-center text-white">NUTRIFY KITCHEN</h2>
+          <div className="imageWrap w-96 h-72 relative">
+            <Image src='/eating.png' alt="Image of a coder" className="rounded-lg p-3" fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
           </div>
-          <p className="text-xl">This blog is to help the coders</p>
-          <h3 className="font-bold text-2xl">Popular Blogs</h3>
+          <div className="flex flex-wrap text-center p-3">
+            <p className="text-base lg:text-xl font-semibold text-white">This blog is to helps you to know the nutritional values of the foods and provide the recipe source for various dishes</p>
+          </div>
           <div className="blogs flex justify-center flex-wrap space-x-5 space-y-3 pb-5">
             <p></p>
 
-            <div className="blogsItems border-2 border-black p-5 h-40">
+            <div className="blogsItems p-5 relative w-64 h-56">
               <div>
-                <h4 className="text-lg font-bold cursor-pointer">How to learn JavaScript in 2024 for web</h4>
-                <p>Here is the resource from where you can learn</p>
+                <Image src='/sandwich.jpg' fill />
               </div>
             </div>
-
-            <div className="blogsItems border-2 border-black p-5 h-40">
+            <div className="blogsItems p-5 relative w-64 h-56">
               <div>
-                <h4 className="text-lg font-bold cursor-pointer">How to learn JavaScript in 2024 for web</h4>
-                <p>Here is the resource from where you can learn</p>
+                <Image src='/veggies and salad.jpg' fill />
               </div>
             </div>
-
-            <div className="blogsItems border-2 border-black p-5 h-40">
+            <div className="blogsItems p-5 relative w-64 h-56">
               <div>
-                <h4 className="text-lg font-bold cursor-pointer">How to learn JavaScript in 2024 for web</h4>
-                <p>Here is the resource from where you can learn</p>
+                <Image src='/variety food.jpg' fill />
+              </div>
+            </div>
+            <div className="blogsItems p-5 relative w-64 h-56">
+              <div>
+                <Image src='/vegetables.jpg' fill />
+              </div>
+            </div>
+            <div className="blogsItems p-5 relative w-64 h-56">
+              <div>
+                <Image src='/Watermelon.webp' fill />
               </div>
             </div>
 
