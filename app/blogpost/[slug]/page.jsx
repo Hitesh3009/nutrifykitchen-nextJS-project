b0d413fb7ee19923a22dc8bc4202d2e19a36ac5a
Hitesh3009 <hiteshbhosle3009@gmail.com>
2024-08-04 22:26:45 +0530

    designed continue for the ui for getblog

--- app/blogpost/[slug]/page.jsx
+++ app/blogpost/[slug]/page.jsx
@@ -27,21 +27,21 @@ export default async function Blogpost({ params }) {
           <Navbar />
 
           <div className="main flex justify-center flex-col items-center space-y-4">
-            <div className="container flex justify-start mt-6 w-6/12 p-4 border-2 border-gray-400">
-              <div className="Imgcontainer border-2 border-black relative w-52 h-52 flex">
+            <div className="container flex justify-start mt-6 w-10/12 md:w-10/12 xl:w-6/12 lg:w-7/12 p-4 border-2 border-gray-400">
+              <div className="Imgcontainer border-2 border-black relative w-44 h-44 md:w-48 md:h-48 lg:w-52 lg:h-52 flex">
                 <Image src={blogImg} fill />
               </div>
-              <div className="source absolute right-1/3 flex flex-col">
-                <span className="text-xl text-green-600">{myBlog.source}</span>
-                <span>See full recipe on : <a href={myBlog.url} className="hover:underline">{myBlog.source}</a></span>
+              <div className="source absolute right-[11%] md:top-[20%] lg:top-[22%] md:right-[14%] lg:right-[27%] flex flex-col ">
+                <span className=" text-xs xl:text-xl md:text-sm lg:text-base text-green-600">{myBlog.source}</span>
+                <span className="text-xs md:text-sm">See full recipe on : <a href={myBlog.url} className="hover:underline cursor-pointer">{myBlog.source}</a></span>
               </div>
-              <div className="flex flex-col justify-center absolute right-1/4 top-1/4">
-                <div className="flex flex-wrap w-[22vw]">
+              <div className="flex flex-col justify-center relative right-[-6%] top-[7%] md:right-[-13%] md:top-[20%] lg:right-[-4.5%] lg:top-[20%] xl:right-[-7.4%] xl:top-[25%]">
+                <div className="flex flex-wrap w-[50vw] md:w-[47vw] xl:w-[30vw] lg:w-[35vw]">
                   {
                     myBlog.healthLabels.map((val, index) => {
                       return (<>
                         <div className="flex flex-col" key={index}>
-                          <p className="mx-2 font-semibold text-sm">{val}</p>
+                          <p className="mx-2 font-semibold text-xs md:text-sm text-pretty">{val}</p>
                         </div>
                       </>)
                     })
@@ -49,14 +49,14 @@ export default async function Blogpost({ params }) {
                 </div>
               </div>
             </div>
-            <div className="w-6/12 flex flex-wrap flex-col justify-start border-2 border-gray-400">
-              <p className="text-center font-bold text-xl mt-3">Ingredients:</p>
+            <div className=" w-10/12 md:w-10/12 xl:w-6/12 lg:w-7/12 flex flex-wrap flex-col justify-start border-2 border-gray-400">
+              <p className="text-center font-bold text-lg lg:text-xl mt-3">Ingredients:</p>
               <div className="p-7 flex flex-wrap">
                 {
                   myBlog.ingredientLines.map((val, index) => {
                     return (<>
                       <div className="flex flex-col" key={index}>
-                        <p className="m-2 font-light">{val}</p>
+                        <p className="m-2 font-light text-sm md:text-base">{val}</p>
                       </div>
                     </>)
                   })
@@ -64,14 +64,14 @@ export default async function Blogpost({ params }) {
               </div>
               <div>
                 <div className="w-full flex flex-wrap justify-center text-justify">
-                  <p className="font-bold text-xl text-center">Nutrition:</p>
+                  <p className="font-bold text-lg lg:text-xl text-center">Nutrition:</p>
                   <div className="mx-2 flex flex-wrap justify-between ">
                     {
                       myBlog.digest.map((item, index) => {
                         return (<div key={index} className="m-3">
-                          <h3 className="text-lg font-bold">{item.label}</h3>
-                          <p className="font-light">Total Fat: {item.total}{item.unit}</p>
-                          <p className="font-light">Daily Fat: {item.daily}{item.unit}</p>
+                          <h3 className="text-base lg:text-lg font-bold">{item.label}</h3>
+                          <p className="font-light text-sm md:text-base">Total Fat: {item.total}{item.unit}</p>
+                          <p className="font-light text-sm md:text-base">Daily Fat: {item.daily}{item.unit}</p>
                           {
                             item.sub && (
                               <div>
@@ -79,9 +79,9 @@ export default async function Blogpost({ params }) {
                                   item.sub.map((subitem, subindex) => {
                                     return (<div key={subindex} className="ml-6">
                                       <ul className="list-disc">
-                                        <li className="font-semibold">{subitem.label}</li>
-                                        <p className="font-light">Total Fat: {subitem.total}{subitem.unit}</p>
-                                        <p className="font-light">Daily Fat: {subitem.daily}{subitem.unit}</p>
+                                        <li className="font-semibold text-sm lg:text-base">{subitem.label}</li>
+                                        <p className="font-light text-sm md:text-base">Total Fat: {subitem.total}{subitem.unit}</p>
+                                        <p className="font-light text-sm md:text-base">Daily Fat: {subitem.daily}{subitem.unit}</p>
                                       </ul>
                                     </div>)
                                   })
